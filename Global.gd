fe8bcc0e87b7de6aede50a728e904b659ce38725
KIRtony <ts15@iu.edu>
2020-12-13 10:05:18 +0700

    1

--- Global.gd
+++ Global.gd
@@ -13,8 +13,10 @@ var saves = [
 	
 ]
 
-
-
+var levels = [
+	"res://Levels/Level1.tscn"
+	,"res://Levels/Level2.tscn"
+]
 func _ready( ):
 	pause_mode = Node.PAUSE_MODE_PROCESS
 
@@ -45,60 +47,87 @@ func decrease_lives(l):
 
 		get_tree().change_scene("res://Levels/Game_Over.tscn")
 
-func get_save_data( ):
+func get_save_data():
 	var data = {
-		"score" :score
-		,"lives" :lives
-		,"health" :health
-		,"level" :level
-		,"player":"" 
-		,"enemy_ground":[] 
+		"score":score
+		,"lives":lives
+		,"level":level
+		,"player":""
+		,"enemy_ground":[]
 		,"enemy_flying":[]
-		,"coins":[ ]
+		,"coins":[]
 	}
-	var player = get_node_or_null("/root/Game/Player.Container/Player" ) 
-	if player!= null:
+	var player = get_node_or_null("/root/Game/Player_Container/Player")
+	if player != null:
 		data["player"] = var2str(player.position)
-	var enemies = get_node("/root/Game/Enemy_Container").get_children() 
+	var enemies = get_node("/root/Game/Enemy_Container").get_children()
 	for e in enemies:
-		if e.is_in_group("Enemy_ Ground"):
-			var temp = {"position":e.position,"max_constraint":e.max_constraint,"min_constraint" :e.min_constraint}
+		if e.is_in_group("Enemy_Ground"):
+			var temp = {"position":var2str(e.position), "max_constraint": e.max_constraint, "min_constraint":e.min_constraint}
 			data["enemy_ground"].append(temp)
 		if e.is_in_group("Enemy_Flying"):
-			var temp = {"position" :e.position}
-			data["enemy_flying"].append(temp )
-	var coins = get_node("/root/Game/Coin_ Container").get_children( )
-	for C in coins:
-		var temp = {"position" :C.position,"score":C.score}
-		data["coin"].append(temp)
+			var temp = {"position":var2str(e.position)}
+			data["enemy_flying"].append(temp)
+	var coins = get_node("/root/Game/Coin_Container").get_children()
+	for c in coins:
+		var temp = {"position":var2str(c.position), "score":c.score}
+		data["coins"].append(temp)
 	print(data)
-	return(data)
+	return data
+	
+func load_save_level(data):
+	score = data["score"]
+	lives = data["lives"]
+	level = data["level"]
+	
+	get_tree().change_scene(levels[level])
+	call_deferred("load_save_data", data)
 	
 func load_save_data(data):
-	pass
+	var menu = get_node_or_null("/root/Game/CanvasLayer/Menu")
+	if menu != null:
+		menu.show()
 	
+	if data["player"] != "":
+		var player = get_node_or_null("/root/Game/Player_Container/Player")
+		if player != null:
+			player.name = "Player2"
+			player.queue_free()
+		get_node("/root/Game/Player_Container").spawn(str2var(data["player"]))
+	var enemy_container = get_node("/root/Game/Enemy_Container")
+	for e in enemy_container.get_children():
+		e.queue_free()
+	for e in data["enemy_ground"]:
+		var attr = {"max_constraint":e["max_constraint"], "min_constraint":e["min_constraint"]}
+		enemy_container.spawn("Enemy_Ground", attr, str2var(e["position"]))
+	for e in data["enemy_flying"]:
+		var attr = {}
+		enemy_container.spawn("Enemy_Flying", attr, str2var(e["position"]))
 	
-	
+	var coint_container = get_node("/root/Game/Coin_Container")
+	for c in coint_container.get_children():
+		c.queue_free()
+	for c in data["coins"]:
+		var attr = {"score":c["score"]}
+		coint_container.spawn(attr, str2var(c["position"]))
 
-
-		
 func save_game(which_file):
-	var file = File.new( )
-	var data = {}
-	file.open(saves[which_file],File.WRITE)
+	var file = File.new()
+	var data = get_save_data()
+	file.open(saves[which_file], File.WRITE)	
 	file.store_string(to_json(data))
-	file.close( )
-
+	file.close()
+	
 func load_game(which_file):
-	var file = File.new( )
+	var file = File.new()
 	if file.file_exists(saves[which_file]):
-		file.open(saves[which_file],File.READ )
-		var data = parse_json(file.get.as_ext( ))
-		file.close( )
+		file.open(saves[which_file], File.READ)
+		var data = parse_json(file.get_as_text())
+		file.close()
 		if typeof(data) == TYPE_DICTIONARY:
-			pass
+			load_save_level(data)
 		else:
-			printerr("Corrupted data")
+			print("Corrupted data")
 	else:
-		printerr("No saved data")
+		print("No saved data")
 
